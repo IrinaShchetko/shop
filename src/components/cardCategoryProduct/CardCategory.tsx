120f326b8909d6b4be8b7d97fc153a3d11a597f8
Irina Shchetko <irina.shetko92@gmail.com>
2023-11-06 05:18:04 +0300

    added styles for home page using grid-area

--- src/components/cardCategoryProduct/CardCategory.tsx
+++ src/components/cardCategoryProduct/CardCategory.tsx
@@ -1,5 +1,5 @@
 import { CatalogProps } from '../../shared/api/types'
-// import styles from './category.module.css'
+import styles from './category.module.css'
 
 interface CategoryProps {
     item: CatalogProps
@@ -8,9 +8,9 @@ interface CategoryProps {
 export const CardCategory: React.FC<CategoryProps> = ({ item }) => {
     return (
         <>
-            <div key={item.id}>
-                <img src={item.img} alt="" />
-                <p>{item.category}</p>
+            <div className={styles.category} key={item.id}>
+                <img className={styles.img} src={item.img} alt="" />
+                <p className={styles.desc}>{item.category}</p>
             </div>
         </>
     )
