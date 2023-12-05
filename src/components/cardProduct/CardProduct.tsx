4f60b7f2a68360a567e5c451bc1724bd48b61258
Irina Shchetko <irina.shetko92@gmail.com>
2023-12-06 01:06:03 +0300

    added carousel for images of products, changed modal

--- src/components/cardProduct/CardProduct.tsx
+++ src/components/cardProduct/CardProduct.tsx
@@ -2,34 +2,36 @@ import unliked from '../../assets/main/unliked.svg'
 import liked from '../../assets/main/liked.svg'
 import loupe from '../../assets/main/loupe.svg'
 import styles from './product.module.css'
-import { GoodsProps } from '../../shared/api/types'
+import { CardProductProps } from '../../shared/api/types'
 import { Button } from '../button/index'
-
-interface CardProductProps {
-  item: GoodsProps
-  onFavoriteClick: (itemId: number | string, isCurrentlyFavorite: boolean) => void
-  onBasketClick: (itemId: number | string, isCurrentlyInBasket: boolean) => void
-  isFavorite?: boolean
-  isInBasket?: boolean
-}
+import { useState } from 'react'
+import { Modal } from '../modal'
+import { ProductImageCarousel } from '../productImageCarousel'
 
 export const CardProduct: React.FC<CardProductProps> = ({ item, onFavoriteClick, onBasketClick, isFavorite = false, isInBasket = false }) => {
   const firstImage = Array.isArray(item.images) ? item.images[0] : item.images
-  //TODO add alt
+  const [isImageModalOpen, setIsImageModalOpen] = useState(false)
+  const toggleImageModal = () => {
+    setIsImageModalOpen(!isImageModalOpen)
+  }
+
   return (
     <div className={styles.product}>
       <img className={styles.img} src={firstImage} alt={item.title} />
       <Button className={styles.favorites} item={item} typeButton="favorites" onButtonClick={onFavoriteClick}>
         <img src={isFavorite ? liked : unliked} alt={isFavorite ? liked : unliked} />
       </Button>
-      <button className={styles.loupe}>
-        <img src={loupe} alt="" />
+      <button className={styles.loupe} onClick={toggleImageModal}>
+        <img src={loupe} alt="loupe to enlarge images" />
       </button>
       <p className={styles.title}>{item.title}</p>
       <p className={styles.price}> BYN {item.price}</p>
       <Button className={styles.basket} item={item} typeButton="basket" onButtonClick={onBasketClick}>
         {isInBasket ? 'Remove from cart' : 'Add to cart'}
       </Button>
+      <Modal open={isImageModalOpen} onClose={toggleImageModal}>
+        <ProductImageCarousel images={Array.isArray(item.images) ? item.images : [item.images]} />
+      </Modal>
     </div>
   )
 }
