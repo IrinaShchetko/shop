47dc3a70ebdc84a636796b5313621675e2a1d158
Irina Shchetko <irina.shetko92@gmail.com>
2023-12-06 15:41:18 +0300

    added pagination

---
+++ src/components/pagination/Pagination.tsx
@@ -0,0 +1,76 @@
+import _ from 'lodash'
+import styles from './styles.module.css'
+
+interface PaginationProps {
+  count: number
+  page: number
+  setPage: (page: number) => void
+  currentPage: number
+}
+
+export const AppPagination = ({ count, page, setPage, currentPage }: PaginationProps) => {
+  const total = Math.ceil(count / page)
+  const pages = _.range(1, total + 1)
+
+  if (count <= 5) {
+    return null
+  }
+
+  const cropPages = (pages: number[]) => {
+    if (pages.length <= 5) {
+      return pages
+    }
+
+    if (currentPage <= 2) {
+      return [...pages.slice(0, 3), '...', total]
+    }
+
+    if (currentPage >= total - 2) {
+      return [1, '...', ...pages.slice(total - 3, total)]
+    }
+
+    return [1, '...', ...pages.slice(currentPage - 1, currentPage + 1), '...', total]
+  }
+
+  const pagesCrop: (number | string)[] = cropPages(pages)
+
+  return (
+    <div className={styles.pagination}>
+      <div className={`${styles.paginationItem} ${currentPage === 1 ? styles.disabled : ''}`} onClick={() => setPage(1)}>
+        First
+      </div>
+      <div
+        className={`${styles.paginationItem} ${currentPage === 1 ? styles.disabled : ''}`}
+        onClick={() => {
+          if (currentPage > 1) {
+            setPage(currentPage - 1)
+          }
+        }}
+      >
+        Prev
+      </div>
+      {pagesCrop.map((pageNumber, index) => (
+        <div
+          key={index}
+          className={`${styles.paginationItem} ${pageNumber === currentPage ? styles.active : ''}`}
+          onClick={() => (typeof pageNumber === 'number' ? setPage(pageNumber) : null)}
+        >
+          {typeof pageNumber === 'number' ? pageNumber : '...'}
+        </div>
+      ))}
+      <div
+        className={`${styles.paginationItem} ${currentPage === total ? styles.disabled : ''}`}
+        onClick={() => {
+          if (currentPage < total) {
+            setPage(currentPage + 1)
+          }
+        }}
+      >
+        Next
+      </div>
+      <div className={`${styles.paginationItem} ${currentPage === total ? styles.disabled : ''}`} onClick={() => setPage(total)}>
+        Last
+      </div>
+    </div>
+  )
+}
--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { CardProduct } from '../../components/cardProduct'
 import { useAppDispatch, useAppSelector } from '../../shared/hooks/useRedux'
@@ -7,6 +7,7 @@ import { fetchGoodsThunk } from '../../redux/goods/thunk'
 import styles from './products.module.css'
 import { addToBasketAsync, addToFavoritesAsync, removeFromBasketAsync, removeFromFavoritesAsync } from '../../redux'
 import { useActionForButton } from '../../shared/hooks/useActionForButton'
+import { AppPagination } from '../../components/pagination'
 
 export const Products = () => {
   const { category } = useParams()
@@ -21,11 +22,17 @@ export const Products = () => {
   }, [dispatch, category])
 
   const filterData = goods.filter(item => item.category === category) //фильтр товаров по категории
+  const pageSize = 6 //кол-во товаров на странице
+  const [currentPage, setCurrentPage] = useState(1)
+  const totalCount = filterData.length
+  const isDataAvailable = totalCount > 0 && filterData.length > 0
+
+  const currentPageData = filterData.slice((currentPage - 1) * pageSize, currentPage * pageSize)
 
   return (
     <div className="container">
       <section className={styles.products}>
-        {filterData.map(item => (
+        {currentPageData.map(item => (
           <CardProduct
             key={item._id}
             item={item}
@@ -50,6 +57,11 @@ export const Products = () => {
           />
         ))}
       </section>
+      {isDataAvailable ? (
+        <AppPagination count={filterData.length} page={pageSize} currentPage={currentPage} setPage={setCurrentPage} />
+      ) : (
+        <p>No products found in this category</p>
+      )}
     </div>
   )
 }
