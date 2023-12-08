95cb7868d250defa2d4214fabca727ca4380fd97
Irina Shchetko <irina.shetko92@gmail.com>
2023-12-08 16:03:08 +0300

    added URL for connect to database

--- shop_API/routes/routeBasket.js
+++ shop_API/routes/routeBasket.js
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 import { MongoClient, ObjectId } from 'mongodb'
 export const routerBasket = Router()
-const url = 'mongodb://127.0.0.1:27017/zebra/'
+const url = 'mongodb+srv://irinashetko92:3215eras@cluster0.ty7dnme.mongodb.net/zebra?retryWrites=true&w=majority'
 const mongoClient = new MongoClient(url)
 
 async function runBasket() {
