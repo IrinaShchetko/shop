983f1176e97d0521fcc2f63396a761be19f55861
Irina Shchetko <irina.shetko92@gmail.com>
2023-12-15 05:41:55 +0300

    added styles for page of product

--- src/components/cardDetail/desc/Desc.tsx
+++ src/components/cardDetail/desc/Desc.tsx
@@ -3,14 +3,39 @@ import liked from '../../../assets/main/liked.svg'
 import { DescProps } from '../../../shared/api/types'
 import styles from './styles.module.css'
 import { ButtonFavAndBasket } from '../../buttonFavAndBasket'
-import { ProductImageCarousel } from '../../productImageCarousel'
+import React from 'react'
 
-export const Desc = ({ product, onBasketClick, onFavoriteClick, isFavorite = false, isInBasket = false }: DescProps) => {
+export const Desc: React.FC<DescProps> = ({ product, onBasketClick, onFavoriteClick, isFavorite, isInBasket, images }) => {
+  let imageArray: string[] = []
+
+  if (typeof images === 'string') {
+    imageArray = [images]
+  } else if (Array.isArray(images)) {
+    imageArray = images
+  }
+
+  if (!imageArray || imageArray.length === 0) {
+    return null
+  }
+
+  const [selectedImage, setSelectedImage] = React.useState(imageArray[0])
+
+  const handleImageClick = (image: string) => {
+    setSelectedImage(image)
+  }
+
   return (
     <section className={styles.desc}>
-      <div className={styles.gallery}>
-        <ProductImageCarousel images={Array.isArray(product.images) ? product.images : [product.images]} />
+      <div className={styles.imagesGallery}>
+        {imageArray.map((image, index) => (
+          <img key={index} src={image} alt={`photo number ${index}`} className={styles.image} onClick={() => handleImageClick(image)} />
+        ))}
       </div>
+
+      <div className={styles.mainImage}>
+        <img src={selectedImage} alt="Main" className={styles.selectedImage} />
+      </div>
+
       <div className={styles.info}>
         <h3 className={styles.title}>{product.title}</h3>
         <ButtonFavAndBasket className={styles.favorites} item={product} typeButton="favorites" onButtonClick={onFavoriteClick}>
@@ -19,23 +44,21 @@ export const Desc = ({ product, onBasketClick, onFavoriteClick, isFavorite = fal
         <ButtonFavAndBasket className={styles.basket} item={product} typeButton="basket" onButtonClick={onBasketClick}>
           {isInBasket ? 'Remove from cart' : 'Add to cart'}
         </ButtonFavAndBasket>
-        {product.size.map((item, index) => (
-          <div key={index} className={styles.sizesContainer}>
-            <p className={styles.size}>{item}</p>
-          </div>
-        ))}
-        <p>Rating</p>
-        <div>
-          <span>Product Information:</span>
-          <span>Fabric composition:</span>
-          <span>{product['fabric structure']}</span>
-          <span>Fabric density:</span>
-          <span>{product['fabric density']}</span>
-          <span>{product['washing instructions']}</span>
+        <div className={styles.sizesContainer}>
+          {product.size.map((item, index) => (
+            <p className={styles.size} key={index}>
+              {item}
+            </p>
+          ))}
         </div>
-        <span>{product.price}</span>
-        <p>{product.desc}</p>
-        <span>{product.vendor_code}</span>
+        <div className={styles.characteristics}>
+          <span className={styles.characteristics__title}>Product Information:</span>
+          <div className={styles.characteristics__structure}>{`Fabric composition: ${product['fabric structure']}`}</div>
+          {product['fabric density'] ? <div className={styles.characteristics__density}>{`Fabric density: ${product['fabric density']}`}</div> : null}
+          {product['washing instructions'] ? <span className={styles.characteristics__recommendation}>{product['washing instructions']}</span> : null}
+        </div>
+        <span className={styles.price}>{`BYN ${product.price}`}</span>
+        <p className={styles.description}>{product.desc}</p>
       </div>
     </section>
   )
