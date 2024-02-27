0ab6390ac41dbd147ee80516adc55bb4b5e83131
Irina Shchetko <irina.shetko92@gmail.com>
2024-02-27 16:29:59 +0300

    refactor: changed png to webp

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo } from 'react'
 import styles from './style.module.css'
 import { createPortal } from 'react-dom'
 import { ModalProps } from '../../shared/api/types'
-import background from '../../assets/header/form_background.png'
+import background from '../../assets/header/form_background.webp'
 
 const modalRootElement = document.querySelector('#modal')
 export const Modal = ({ open, onClose, children }: ModalProps) => {
